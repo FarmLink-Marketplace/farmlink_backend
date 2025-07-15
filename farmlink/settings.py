5390fa656b87a8f9c88cbe4c444625908b5fa521
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2025-07-15 15:29:41 +0100

    Changed database to Neon and installed gunicorn in preparation for deploying on render

--- farmlink/settings.py
+++ farmlink/settings.py
@@ -91,14 +91,14 @@ TEMPLATES = [
 WSGI_APPLICATION = "farmlink.wsgi.application"
 
 AUTH_USER_MODEL = "api.User"
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
-# DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL"))}
+DATABASES = {"default": dj_database_url.parse(os.getenv("DATABASE_URL"))}
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
